ce73936ee8f8b3884a9542d37486a085c14955b8
Pedrophobia <pedro.amachado167@gmail.com>
2026-09-23 10:54:52 -0300

    Correcao das imagens do json

--- pawdex/src/pages/Home.jsx
+++ pawdex/src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="page-container">
       <h2> Guia de Criaturas</h2>
-      <p>Explore as espécies, verifique os seus atributos.</p>
+      <p>Explore as espécies, verifique os seus atributos (esta faltando MUITOS pawls e alguns pawls estao errados).</p>
       <Link to="/listagem" className="btn-primary">
         Listagem
       </Link>
